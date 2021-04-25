35196d91d67efe48c8f1619befc24acec005a11c
mahidulmoon <mahidulmoon@gmail.com>
2021-04-25 19:58:10 +0600

    packages for frontend

--- app/src/App.js
+++ app/src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+import React from 'react';
+import ApolloClient from 'apollo-boost';
+import { ApolloProvider } from '@apollo/react-hooks';
+const client = new ApolloClient({
+  uri: 'http://localhost:8000/graphql/', // your GraphQL Server 
+});
+const App = () => (
+  <ApolloProvider client={client}>
+    <div style={{
+      backgroundColor: '#00000008',
+      display: 'flex',
+      justifyContent:'center',
+      alignItems:'center',
+      height: '100vh',
+      flexDirection: 'column',
+    }}>
+      <h2>My first Apollo app <span role="img" aria-label="rocket">🚀</span></h2>
     </div>
-  );
-}
-
+  </ApolloProvider>
+);
 export default App;
